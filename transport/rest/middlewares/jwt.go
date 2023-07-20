1be4cfe6fa256bbcea162d2b7e46517341bf686d
yogabagas <ybagasakthi@gmail.com>
2023-07-20 20:57:32 +0700

    bypass API register users for authentication

--- transport/rest/middlewares/jwt.go
+++ transport/rest/middlewares/jwt.go
@@ -3,33 +3,49 @@ package middlewares
 import (
 	"errors"
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
 	"github/yogabagas/print-in/config"
 	"github/yogabagas/print-in/transport/rest/handler/response"
 	"net/http"
 	"strings"
+
+	"github.com/dgrijalva/jwt-go"
 )
+
+type MiddlewareImpl struct{}
+
+type Middleware interface {
+	AuthenticationMiddleware(next http.Handler) http.Handler
+}
+
+func NewMiddleware() Middleware {
+	return &MiddlewareImpl{}
+}
 
 var res = response.NewJSONResponse()
 
 // AuthenticationMiddleware validates the JWT token.
-func AuthenticationMiddleware(next http.Handler) http.Handler {
+func (mi *MiddlewareImpl) AuthenticationMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("authorization") != "" {
-			if parseJwt(r.Header.Get("authorization")) {
-				next.ServeHTTP(w, r)
-			} else {
-				res.SetError(response.ErrUnauthorized).SetMessage(errors.New("Invalid Authorized Token").Error()).Send(w)
-				return
-			}
-		} else {
+
+		token := r.Header.Get("Authorization")
+
+		isRegister := r.URL.Path == "/v1/users" && r.Method == http.MethodPost
+
+		if token == "" && !isRegister {
 			res.SetError(response.ErrUnauthorized).SetMessage(errors.New("An Authorization Header is required").Error()).Send(w)
 			return
 		}
+
+		if !mi.parseJwt(token) && !isRegister {
+			res.SetError(response.ErrUnauthorized).SetMessage(errors.New("Invalid Authorized Token").Error()).Send(w)
+			return
+		}
+
+		next.ServeHTTP(w, r)
 	})
 }
 
-func parseJwt(authorizationHeader string) (valid bool) {
+func (mi *MiddlewareImpl) parseJwt(authorizationHeader string) (valid bool) {
 	bearerToken := strings.Split(authorizationHeader, " ")
 	if len(bearerToken) == 2 {
 		token, _ := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
