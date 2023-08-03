3db1a6c4c42dc1c58ad351f0c0fd3764464705e8
yogabagas <ybagasakthi@gmail.com>
2023-08-03 20:40:37 +0700

    addd content-type

--- transport/rest/middlewares/cors.go
+++ transport/rest/middlewares/cors.go
@@ -5,6 +5,8 @@ import "net/http"
 func (m *MiddlewareImpl) CORSHandle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		w.Header().Set("Content-Type", "application/json")
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		w.Header().Set("Access-Control-Allow-Methods", "*")
