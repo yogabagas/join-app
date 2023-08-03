c7fe3dafb4b66d34071e6f60ac7ae111c35e7f16
yogabagas <ybagasakthi@gmail.com>
2023-08-03 21:33:07 +0700

    handle CORS

--- transport/rest/handler/healthz.go
+++ transport/rest/handler/healthz.go
@@ -8,11 +8,7 @@ import (
 
 func (h *HandlerImpl) Healthcheck(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-
-	if r.Method != http.MethodGet || r.Method != http.MethodOptions {
+	if r.Method != http.MethodGet {
 		http.Error(w, "wrong method", http.StatusMethodNotAllowed)
 		return
 	}
